58591fcb2e8e922f3a75b13fd83377db0d82660e
brittany-johnson <brittanyj.email@gmail.com>
2017-07-13 12:38:08 -0500

    i fixed it patrick didn't

--- app.js
+++ app.js
@@ -112,19 +112,3 @@ app.get("/likes", function(req, res) {
     res.redirect('/login')
   }
 })
-
-
-//PATRICK STUFF
-var pg = require('pg');
-
-pg.defaults.ssl = true;
-pg.connect(process.env.HEROKU_POSTGRESQL_PURPLE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
