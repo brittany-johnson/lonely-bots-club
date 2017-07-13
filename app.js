b256497c59db961b1e1d5018958fb954dee70a13
brittany-johnson <brittanyj.email@gmail.com>
2017-07-13 12:24:19 -0500

    patrick is trying to get this to work

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(session({
 }))
 
 var port = process.env.PORT || 3000;
+var postgres = process.env
 
 app.listen(port, function() {
   console.log("Is this thing on?")
@@ -111,3 +112,19 @@ app.get("/likes", function(req, res) {
     res.redirect('/login')
   }
 })
+
+
+//PATRICK STUFF
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
